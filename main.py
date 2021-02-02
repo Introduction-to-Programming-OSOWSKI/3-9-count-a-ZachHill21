d6bad24f797c8282b242bebcca347ccb56d4a3e0
ZachHill21 <814055@mystma.org>
2021-02-02 16:16:38 +0000

    try 1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#WRITE YOUR CODE IN THIS FILE
+def countA(w):
+    count = 0
+    for i in range (0 ,len(w)):
+        if w[i] == "a":
+            count = (count + 1) 
+    return count
+print (countA("armadillo"))
